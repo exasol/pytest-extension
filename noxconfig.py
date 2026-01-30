143f6cb39ae73590daff9f9c9b6b197a4a68554a
Sebastian Bär <sebastian.baer@exasol.com>
2026-01-30 12:54:45 +0100

    #2: Updated noxconfig.py with `root_path` and `project_name`.

--- noxconfig.py
+++ noxconfig.py
@@ -42,7 +42,9 @@ class Config(BaseConfig):
 
 
 PROJECT_CONFIG = Config(
-    # Uses SaaS; not ITDE DB versions
+    root_path=Path(__file__).parent,
+    project_name="pytest_extension",
     python_versions=("3.10", "3.11", "3.12", "3.13"),
+    # Uses SaaS; not ITDE DB versions
     exasol_versions=(),
 )
